b2fcb423a81cbd11e797ab64afce122a2957f39c
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2024-03-14 13:43:37 -0700

    Define node iterator functions

---
+++ include/node-iterator.hpp
@@ -0,0 +1,42 @@
+#ifndef NODE_ITERATOR_H
+#define NODE_ITERATOR_H
+
+#include "node.hpp"
+#include <iterator>
+
+template <typename T>
+class NodeIterator : public std::iterator<std::bidirectional_iterator_tag, T> {
+public:
+  NodeIterator();
+  NodeIterator(Node<T> *current);
+
+  // ++ prefix
+  NodeIterator &operator++();
+
+  // ++ postfix
+  NodeIterator operator++(int);
+
+  // -- prefix
+  NodeIterator &operator--();
+
+  // -- postfix
+  NodeIterator operator--(int);
+
+  // dereference
+  T &operator*();
+
+  // equal to
+  template <typename U>
+  friend bool operator==(const NodeIterator<U> &lhs,
+                         const NodeIterator<U> &rhs);
+
+  // not equal
+  template <typename U>
+  friend bool operator!=(const NodeIterator<U> &lhs,
+                         const NodeIterator<U> &rhs);
+
+private:
+  Node<T> *current;
+};
+
+#endif // !NODE_ITERATOR_H
